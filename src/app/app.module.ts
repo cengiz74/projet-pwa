fb344baba3da20f7146eccf6a5a8efaee9db451e
d_berardozzi <d.berardozzi@inforum-pos.com>
2018-01-19 18:47:41 +0100

    Initial commit !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,25 @@ import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
 
+import { CovalentLayoutModule, CovalentStepsModule /*, any other modules */ } from '@covalent/core';
+// (optional) Additional Covalent Modules imports
+import { CovalentHttpModule } from '@covalent/http';
+import { CovalentHighlightModule } from '@covalent/highlight';
+import { CovalentMarkdownModule } from '@covalent/markdown';
+import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CovalentLayoutModule,
+    CovalentStepsModule,
+    // (optional) Additional Covalent Modules imports
+    CovalentHttpModule.forRoot(),
+    CovalentHighlightModule,
+    CovalentMarkdownModule,
+    CovalentDynamicFormsModule,
     BrowserModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
